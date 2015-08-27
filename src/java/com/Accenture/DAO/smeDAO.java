bb2d1e0882eee31f23557208866cc3e010ab451e
nsizwa <nsizwa.mhlongo@gmail.com>
2015-08-27 09:53:46 +0300

    adding sme

---
+++ src/java/com/Accenture/DAO/smeDAO.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Accenture.DAO;
+
+import com.Accenture.Model.sme;
+import com.Accenture.Model.trainerpojo;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate4.HibernateTemplate;
+
+/**
+ *
+ * @author EDU-Stud
+ */
+public class smeDAO {
+    HibernateTemplate template;  
+public void setTemplate(HibernateTemplate template) {  
+    this.template = template;  
+    this.template.setCheckWriteOperations(false);
+}  
+//method to save Trainer  
+public void savesme(sme e){  
+    template.save(e);  
+}  
+//method to update Trainer  
+public void updatesme(sme e){  
+    template.update(e);  
+}  
+//method to delete Trainer  
+public void deletesme(sme e){  
+    template.delete(e);  
+}  
+
+public void updateforce (sme l){
+   Session session= null; 
+   SessionFactory sessionFactory = new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
+   session =sessionFactory.openSession();
+  session.beginTransaction(); 
+
+        session.update(l);
+        session.getTransaction().commit();
+         session.flush();
+    session.close();
+}
+public void deleteforce (sme l){
+  Session session= null; 
+   SessionFactory sessionFactory = new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
+   session =sessionFactory.openSession();
+  session.beginTransaction(); 
+
+        session.delete(l);
+        session.getTransaction().commit();
+        session.flush();
+    session.close();
+}
+
+
+//method to return one Trainer of given id  
+public sme getById(int id){  
+    sme e=(sme)template.get(sme.class,id);  
+    return e;  
+}  
+//method to return all Trainer  
+public List<sme> getsme(){  
+    List<sme> list=new ArrayList<sme>();  
+    list=template.loadAll(sme.class);  
+    return list;  
+}
+  //method to return one employee of given email  
+public List<Object> getsmeByEmail(String email){  
+   List<Object>list=new ArrayList<>();
+   String query="from sme sme where email =?";
+   list=template.find(query,email);
+    return list;  
+}
+public String checklogin(String email,String password){
+    String e="",p="",msg="";
+    Session ses=null;
+    SessionFactory sf=new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
+    ses=sf.openSession();
+    ses.beginTransaction();
+    String HQL_QUERY="from sme sme where email =:email and password =:password";
+    org.hibernate.Query query=ses.createQuery(HQL_QUERY);
+    query.setParameter("email",email);
+    query.setParameter("password",password);
+
+    for(Iterator it=query.iterate();it.hasNext();){
+             trainerpojo b=(trainerpojo) it.next();
+             e=b.getEmail();
+             p=b.getPassword();
+    }
+    if(e.equals(email)&&p.equals(password)){
+        msg="yes";
+    }
+    
+return msg;
+}
+public List<sme>findlocandgro(String email){
+    List<sme>list=new ArrayList<>();
+    Session ses=null;
+    SessionFactory sf=new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
+    ses=sf.openSession();
+    ses.beginTransaction();
+    String HQL_QUERY="from sme sme where email =:email";
+    org.hibernate.Query query=ses.createQuery(HQL_QUERY);
+    query.setParameter("email",email);
+    for(Iterator it=query.iterate();it.hasNext();){
+             sme b=(sme) it.next();
+          
+             list.add(b);
+    }
+    return list;
+}
+
+}
---
+++ src/java/com/Accenture/Model/sme.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Accenture.Model;
+
+/**
+ *
+ * @author EDU-Stud
+ */
+public class sme {
+    private int smeid;
+    private String companyname;
+    private String companyaddress;
+    private String companyemail;
+    private String companycontact;
+    private String companypassword;
+    private String companylocation;
+    private int companygroupid;
+
+    /**
+     * @return the smeid
+     */
+    public int getSmeid() {
+        return smeid;
+    }
+
+    /**
+     * @param smeid the smeid to set
+     */
+    public void setSmeid(int smeid) {
+        this.smeid = smeid;
+    }
+
+    /**
+     * @return the companyname
+     */
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    /**
+     * @param companyname the companyname to set
+     */
+    public void setCompanyname(String companyname) {
+        this.companyname = companyname;
+    }
+
+    /**
+     * @return the companyaddress
+     */
+    public String getCompanyaddress() {
+        return companyaddress;
+    }
+
+    /**
+     * @param companyaddress the companyaddress to set
+     */
+    public void setCompanyaddress(String companyaddress) {
+        this.companyaddress = companyaddress;
+    }
+
+    /**
+     * @return the companyemail
+     */
+    public String getCompanyemail() {
+        return companyemail;
+    }
+
+    /**
+     * @param companyemail the companyemail to set
+     */
+    public void setCompanyemail(String companyemail) {
+        this.companyemail = companyemail;
+    }
+
+    /**
+     * @return the companycontact
+     */
+    public String getCompanycontact() {
+        return companycontact;
+    }
+
+    /**
+     * @param companycontact the companycontact to set
+     */
+    public void setCompanycontact(String companycontact) {
+        this.companycontact = companycontact;
+    }
+
+    /**
+     * @return the companypassword
+     */
+    public String getCompanypassword() {
+        return companypassword;
+    }
+
+    /**
+     * @param companypassword the companypassword to set
+     */
+    public void setCompanypassword(String companypassword) {
+        this.companypassword = companypassword;
+    }
+
+    /**
+     * @return the companylocation
+     */
+    public String getCompanylocation() {
+        return companylocation;
+    }
+
+    /**
+     * @param companylocation the companylocation to set
+     */
+    public void setCompanylocation(String companylocation) {
+        this.companylocation = companylocation;
+    }
+
+    /**
+     * @return the companygroupid
+     */
+    public int getCompanygroupid() {
+        return companygroupid;
+    }
+
+    /**
+     * @param companygroupid the companygroupid to set
+     */
+    public void setCompanygroupid(int companygroupid) {
+        this.companygroupid = companygroupid;
+    }
+}
